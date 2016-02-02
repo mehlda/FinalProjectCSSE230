0c2e74d0667bf97d9a9b5615d861a5c5283ba450
mehlda <davidamehl@gmail.com>
2016-02-02 18:06:48 -0500

    Added hello world test

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author David Mehl.
+ *         Created Feb 2, 2016.
+ */
+public class Main {
+
+	/**
+	 * TODO Put here a description of what this method does.
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub.
+		System.out.println("Hello World");
+
+	}
+
+}
