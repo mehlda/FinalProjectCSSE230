e713c3e946e8f258503e5e9932f7fc55f61222cc
vinnyaa <andersva@rose-hulman.edu>
2016-02-09 21:56:50 -0500

    added a new TODO in addNextLocation(Destination d) because i'm unable to access the time cost between destinations. I need a way to figure out which connection i am using to ascertain the correct time cost for the connection so i can add it to the total cost of the route

--- src/Route.java
+++ src/Route.java
@@ -11,12 +11,16 @@ public class Route extends LinkedList<Destination> {
 	/* costs include heuristic values */
 	public int timeCost;
 	public int distanceCost;
+	public DestinationNode first;
+	public DestinationNode last;
 
 	/**
 	 * Constructs a new Route object
 	 */
 	public Route() {
 		// TODO: implement this method
+		this.first = null;
+		this.last = null;
 		timeCost = distanceCost = 0;
 	}
 
@@ -28,13 +32,15 @@ public class Route extends LinkedList<Destination> {
 	 */
 	public Route(Destination destination) {
 		this.add(destination);
+		this.first = new DestinationNode(destination);
 		timeCost = distanceCost = 0; // started here, haven't travelled anywhere
+		this.last = this.first;
 	}
 
 	/**
-	 * Adds a new destination to the end of the Route.
-	 * Updates the time and distance cost to appropriate value based off of the
-	 * connection value to the next location.
+	 * Adds a new destination to the end of the Route. Updates the time and
+	 * distance cost to appropriate value based off of the connection value to
+	 * the next location.
 	 * 
 	 * Does not add heuristic value to cost!
 	 * 
@@ -42,15 +48,31 @@ public class Route extends LinkedList<Destination> {
 	 *            - Destination to add to the end of this Route
 	 * @return true if successfully added
 	 */
-	public boolean addNextLocation(Destination destination) {
-		// TODO: implement this method
-		return false;
+	public boolean addNextLocation(Destination d) {
+		// TODO: This is where I need to be able to find a connection somehow
+		// while only having the last element in the route and the destination
+		// to be inserted
+
+		int oldx = this.last.destination.coordinate.x;
+		int oldy = this.last.destination.coordinate.y;
+
+		this.last.next = new DestinationNode(d);
+		this.last = this.last.next;
+
+		int newx = this.last.destination.coordinate.x;
+		int newy = this.last.destination.coordinate.y;
+
+		int distx = (oldx - newx) * (oldx - newx);
+		int disty = (oldy - newy) * (oldy - newy);
+		int totalDist = (int) Math.sqrt(distx + disty);
+
+		distanceCost += totalDist;
+		return true;
 	}
 
 	/**
 	 * Checks the Route to see if the first element is the starting location and
-	 * if the last element is the ending location.
-	 * If so, it will return true.
+	 * if the last element is the ending location. If so, it will return true.
 	 * This will return false if the route is empty or not complete.
 	 * 
 	 * @param start
@@ -64,5 +86,23 @@ public class Route extends LinkedList<Destination> {
 			return false;
 		return this.getFirst().name.equals(start)
 				&& this.getLast().name.equals(end);
+	}
+
+	public class DestinationNode {
+		private DestinationNode next;
+		private Destination destination;
+
+		public DestinationNode(Destination d) {
+			this.next = null;
+			this.destination = d;
+		}
+
+		public DestinationNode getNext() {
+			return this.next;
+		}
+
+		public Destination getDestination() {
+			return this.destination;
+		}
 	}
 }
