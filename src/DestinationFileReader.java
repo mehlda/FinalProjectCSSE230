9c2aef46719a34a7e6b8edcb749555fe97e866d2
David Mehl <davidamehl@gmail.com>
2016-02-17 20:36:23 -0500

    Added blank line to DestinationReader

--- src/DestinationFileReader.java
+++ src/DestinationFileReader.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 
+
 public class DestinationFileReader {
 	// need to create a file reader. the fields in this class will need to be.
 	public String inputFile = "src/assets/destinations.txt";
