9c7ed072cf1d8b017ec52baada87cc5d89254f5c
David Mehl <davidamehl@gmail.com>
2016-02-21 16:31:06 -0500

    Removed TODOs

--- src/Testing.java
+++ src/Testing.java
@@ -5,94 +5,88 @@ public class Testing {
 
 	@Test
 	public void testCoordinateDistance() {
-		Coordinate[] c1 = { new Coordinate(39.784296, -86.158154),
-				new Coordinate(37.649918, -122.462436),
-				new Coordinate(47.606663, -122.332648),
-				new Coordinate(52.234314, 21.000007),
-				new Coordinate(39.784296, -86.158154)};
-		Coordinate[] c2 = { new Coordinate(30.271201, -97.741139),
-				new Coordinate(38.125674, -121.825022),
-				new Coordinate(35.229555, -80.845007),
-				new Coordinate(41.876630, -87.630071),
-				new Coordinate(39.784296, -86.158154)};
+		Coordinate[] c1 = { new Coordinate(39.784296, -86.158154), new Coordinate(37.649918, -122.462436),
+				new Coordinate(47.606663, -122.332648), new Coordinate(52.234314, 21.000007),
+				new Coordinate(39.784296, -86.158154) };
+		Coordinate[] c2 = { new Coordinate(30.271201, -97.741139), new Coordinate(38.125674, -121.825022),
+				new Coordinate(35.229555, -80.845007), new Coordinate(41.876630, -87.630071),
+				new Coordinate(39.784296, -86.158154) };
 		double[] distances = { 926.60, 47.85, 2280.75, 4666.64, 0 };
 
 		for (int i = 0; i < c1.length; i++) {
-			assertEquals((int) distances[i],
-					(int) c1[i].straightLineDistance(c2[i]));
+			assertEquals((int) distances[i], (int) c1[i].straightLineDistance(c2[i]));
 		}
 	}
 
 	@Test
 	public void testGraphHashValue() {
 		Graph g = new Graph();
-		String[] names = { "A", "b", "C", "d", "E", "f", "G", "h", "I", "j",
-				"K", "l", "M", "n", "O", "p", "Q", "r", "S", "t", "U", "v",
-				"W", "x", "Y", "z" };
+		String[] names = { "A", "b", "C", "d", "E", "f", "G", "h", "I", "j", "K", "l", "M", "n", "O", "p", "Q", "r",
+				"S", "t", "U", "v", "W", "x", "Y", "z" };
 		for (int i = 0; i < names.length; i++) {
 			assertEquals(i, g.getHashValue(names[i]));
 		}
-		
-		String[] names2 = {"asd", "set", "opj", "iub", "qwer"};
-		for(String s : names2) {
+
+		String[] names2 = { "asd", "set", "opj", "iub", "qwer" };
+		for (String s : names2) {
 			System.out.println(g.getHashValue(s));
 		}
 	}
 
 	@Test
 	public void testGraphInsert() {
-		// TODO: test insert()
+		// test insert()
 
 	}
 
 	@Test
 	public void testGraphRemove() {
-		// TODO: test remove()
+		// test remove()
 
 	}
 
 	@Test
 	public void testGraphFind() {
-		// TODO: test find()
+		// test find()
 
 	}
 
 	@Test
 	public void testGraphSuggestions() {
-		// TODO: test findSuggestions()
+		// test findSuggestions()
 
 	}
 
 	@Test
 	public void testRouteCompare() {
-		// TODO: test compareToTime()
-		// TODO: test compareToDistance()
+		// test compareToTime()
+		// test compareToDistance()
 
 	}
 
 	@Test
 	public void testRouteComplete() {
-		// TODO: test isCompleteRoute()
+		// test isCompleteRoute()
 
 	}
 
 	@Test
 	public void testRouteQueueBuild() {
-		// TODO: test buildQueue()
-		// TODO: test peek()
+		// test buildQueue()
+		// test peek()
 
 	}
 
 	@Test
 	public void testRouteQueueAdd() {
-		// TODO: test add()
+		// test add()
 
 	}
 
 	@Test
 	public void testRouteQueuePoll() {
-		// TODO: test poll()
-		// TODO: test peek()
+		// test poll()
+		// test peek()
 
 	}
 }
