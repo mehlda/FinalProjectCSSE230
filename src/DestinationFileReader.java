2d8dfbea072bbab74c5375dffc71e51774bee8ea
David Mehl <davidamehl@gmail.com>
2016-02-21 16:29:28 -0500

    Cleaning house

--- src/DestinationFileReader.java
+++ src/DestinationFileReader.java
@@ -1,5 +1,4 @@
 import java.awt.Point;
-import java.awt.geom.Point2D;
 import java.beans.XMLEncoder;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
@@ -8,172 +7,165 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 
-
-
+/**
+ * Reads the destination file and generates an xml file
+ *
+ * @author Anderson. Created Feb 21, 2016.
+ */
 public class DestinationFileReader {
 	// need to create a file reader. the fields in this class will need to be.
-	public String inputFile = "src/assets/destinations.txt";
-	public String line;
-	public String address;
-	public String name;
-	public String location;
-	
+	private String inputFile = "src/assets/destinations.txt";
+	private String line;
+	private String address;
+	private String name;
+	private String location;
+
 	// this one holds for making connections later
-	public Object[][] neighbors;
-	public String[] neighborNames;
-	public int[] neighborTimeCost;
-	public int[] neighborDistCost;
-	public String[] coord;
-	public int rating;
-	public Double x;
-	public Double y;
-	public int mapx;
-	public int mapy;
-	public Graph newgraph;
-	public int index;
-	
-	
-	
-	
-	public DestinationFileReader(){
-		neighbors = new Object[50][3];
+	private Object[][] neighbors;
+	private String[] neighborNames;
+	private int[] neighborTimeCost;
+	private int[] neighborDistCost;
+	private String[] coord;
+	private int rating;
+	private Double x;
+	private Double y;
+	private int mapx;
+	private int mapy;
+	private Graph newgraph;
+	private int index;
+
+	private DestinationFileReader() {
+		this.neighbors = new Object[50][3];
 		try {
 			this.newgraph = new Graph();
-			
-			FileReader fileReader = new FileReader(inputFile);
-			
+
+			FileReader fileReader = new FileReader(this.inputFile);
+
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
-			
+
 			// actually read the inforation into arrays
-			while((line = bufferedReader.readLine()) != null){
-				String[] lineArray = line.split(":");
-				name = lineArray[0];
-				location = lineArray[1];
-				coord = location.split(",");
-				x = Double.parseDouble(coord[0]);
-				y = Double.parseDouble(coord[1]);
-				address = lineArray[2];
-				rating = Integer.parseInt(lineArray[3]);
+			while ((this.line = bufferedReader.readLine()) != null) {
+				String[] lineArray = this.line.split(":");
+				this.name = lineArray[0];
+				this.location = lineArray[1];
+				this.coord = this.location.split(",");
+				this.x = Double.parseDouble(this.coord[0]);
+				this.y = Double.parseDouble(this.coord[1]);
+				this.address = lineArray[2];
+				this.rating = Integer.parseInt(lineArray[3]);
 				String[] map = lineArray[4].split(",");
-				mapx = Integer.parseInt(map[0]);
-				mapy = Integer.parseInt(map[1]);
-				
+				this.mapx = Integer.parseInt(map[0]);
+				this.mapy = Integer.parseInt(map[1]);
+
 				// second line of the destination read in
 				// holds the names of the neighbors
-				line = bufferedReader.readLine();
-				String[] line2Array = line.split(":");
-				neighborNames = new String[line2Array.length];
+				this.line = bufferedReader.readLine();
+				String[] line2Array = this.line.split(":");
+				this.neighborNames = new String[line2Array.length];
 				int count = 0;
-				for(String names : line2Array){
-					neighborNames[count] = names;
+				for (String names : line2Array) {
+					this.neighborNames[count] = names;
 					count++;
 				}
-				
+
 				// third line of the destination
 				// holds the distance cost of each of the neighbors
-				line = bufferedReader.readLine();
-				String[] line3Array = line.split(":");
-				neighborDistCost = new int[line3Array.length];
+				this.line = bufferedReader.readLine();
+				String[] line3Array = this.line.split(":");
+				this.neighborDistCost = new int[line3Array.length];
 				count = 0;
-				for(String distance : line3Array){
-					neighborDistCost[count] = Integer.parseInt(distance);
+				for (String distance : line3Array) {
+					this.neighborDistCost[count] = Integer.parseInt(distance);
 					count++;
 				}
-				
-				
+
 				// fourth line of the destination
 				// holds the time cost of each of the neighbors
-				line = bufferedReader.readLine();
-				String[] line4Array = line.split(":");
-				neighborTimeCost = new int[line4Array.length];
+				this.line = bufferedReader.readLine();
+				String[] line4Array = this.line.split(":");
+				this.neighborTimeCost = new int[line4Array.length];
 				count = 0;
-				for(String timecost : line4Array){
-					neighborTimeCost[count] = Integer.parseInt(timecost);
+				for (String timecost : line4Array) {
+					this.neighborTimeCost[count] = Integer.parseInt(timecost);
 					count++;
 				}
 				Destination dest = this.makeDestination();
 				System.out.println(dest.name);
-				newgraph.insert(dest);
-				
-				
+				this.newgraph.insert(dest);
+
 			}
 			bufferedReader.close();
-			
+
 		} catch (FileNotFoundException exception) {
 			System.out.println("error loading the program, missing files");
 			exception.printStackTrace();
-		} catch (IOException ex){
+		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
-		
-		this.makeConnections(newgraph);
+
+		this.makeConnections(this.newgraph);
 	}
 
+	private void makeConnections(Graph newgraph) {
+		for (Object[] temp : this.neighbors) {
 
-
-
-	public void makeConnections(Graph newgraph) {
-		for(Object[] temp : neighbors){
-			
 			// first element in each is the string
 			String thisname = (String) temp[0];
-			if(thisname == null) return;
+			if (thisname == null)
+				return;
 			Destination thisdest = newgraph.find(thisname);
-			
+
 			String[] neighNames = (String[]) temp[1];
 			int[] neighDist = (int[]) temp[2];
 			int[] neighTime = (int[]) temp[3];
-			for(int x = 0 ; x < neighNames.length; x++){
+			for (int x = 0; x < neighNames.length; x++) {
 				Destination a = newgraph.find(neighNames[x]);
-				int thistime = (int) neighTime[x];
-				int thisdist = (int) neighDist[x];
-				
-				Connection con = new Connection(thisdest, a,thisdist,thistime);
+				int thistime = neighTime[x];
+				int thisdist = neighDist[x];
+
+				Connection con = new Connection(thisdest, a, thisdist, thistime);
 				System.out.println("Connection: " + thisdest.name + " -> " + a.name);
 				thisdest.addConnection(con);
 			}
-			
-			
+
 		}
 	}
 
-
-
-
 	private Destination makeDestination() {
-		
-		Coordinate thisspot = new Coordinate(x,y);
-		
-		Object[] temp = {this.name, neighborNames, neighborDistCost, neighborTimeCost};
-		
-		neighbors[index] = temp;
-		index ++;
-		
-		Point point = new Point(mapx, mapy);
-		
-		
-		return new Destination(thisspot, name, address, rating, null, point);
+
+		Coordinate thisspot = new Coordinate(this.x, this.y);
+
+		Object[] temp = { this.name, this.neighborNames, this.neighborDistCost, this.neighborTimeCost };
+
+		this.neighbors[this.index] = temp;
+		this.index++;
+
+		Point point = new Point(this.mapx, this.mapy);
+
+		return new Destination(thisspot, this.name, this.address, this.rating, null, point);
 	}
-	
-	public static void main(String[] args){
+
+	/**
+	 * Begins generation of xml file
+	 *
+	 * @param args
+	 */
+	private static void main(String[] args) {
 		DestinationFileReader xxl = new DestinationFileReader();
 		Graph thisgraph = xxl.newgraph;
-		
+
 		try {
 			xxl.write(thisgraph, "src/assets/graph.xml");
 		} catch (Exception exception) {
 			exception.printStackTrace();
 		}
-		
+
 	}
-	
-	public static void write(Graph g, String filename) throws Exception {
-		XMLEncoder encoder = 
-				new XMLEncoder(
-						new BufferedOutputStream(
-								new FileOutputStream(filename)));
+
+	private static void write(Graph g, String filename) throws Exception {
+		XMLEncoder encoder = new XMLEncoder(new BufferedOutputStream(new FileOutputStream(filename)));
 		encoder.writeObject(g);
 		encoder.close();
 	}
-	
+
 }
